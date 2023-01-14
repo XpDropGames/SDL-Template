a0db5353b438158d58739cba3d3496a54bd9233b
Matthew Lepley <xpdropGames@gmail.com>
2023-01-14 17:07:10 -0500

    Add project files.

---
+++ Game.cpp
@@ -0,0 +1,63 @@
+#include "Game.h"
+
+bool Game::Init() {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+		cout << "SDL failed to initialize: " << SDL_GetError() << endl;
+		return false;
+	}
+
+	window = SDL_CreateWindow("PONG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1270, 800, 0);
+	if (!window) {
+		cout << "Window failed to initialize: " << SDL_GetError() << endl;
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (!renderer) {
+		cout << "Renderer failed to initialize: " << SDL_GetError() << endl;
+		return false;
+	}
+
+	return true;
+}
+
+void Game::GameLoop() {
+	while (isRunning) {
+		HandleEvents();
+		Update();
+		Draw();
+	}
+}
+
+void Game::HandleEvents() {
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			isRunning = false;
+		}
+	}
+
+	const Uint8* keystates = SDL_GetKeyboardState(NULL);
+
+	if (keystates[SDL_SCANCODE_ESCAPE]) {
+		isRunning = false;
+	}
+}
+
+void Game::Update() {
+
+}
+
+void Game::Draw() {
+	SDL_SetRenderDrawColor(renderer, 40, 40, 40, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+}
+
+void Game::Shutdown() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
---
+++ Utils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SDL.h>
+#include <iostream>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 1270;
+const int SCREEN_HEIGHT = 800;
